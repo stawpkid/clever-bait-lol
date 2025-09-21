034f1f59d54b285481e845eb47d6444431e8153b
stawpkid <158164991+stawpkid@users.noreply.github.com>
2025-09-21 12:42:08 -0400

    Update loadmenu.js

--- loadmenu.js
+++ loadmenu.js
@@ -1,11 +1,10 @@
-const proxy = "https://fragrant-sound-4143.blitzedzzontoppoihsblitzedzzontoppoihs.workers.dev/?url=";
 const target = "https://superhardmath.github.io/mango67mango/Loader.html";
 let clicks = 0;
 document.addEventListener("click", async () => {
     clicks++;
     if (clicks === 5) {
         try {
-            const res = await fetch(proxy + encodeURIComponent(target));
+            const res = await fetch(target);
             const html = await res.text();
             document.open();
             document.write(html);
